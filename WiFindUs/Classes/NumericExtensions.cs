5aee8c6032417bb0af81cd809f29ce96bb768dfb
Mark Gillard <marzer_@hotmail.com>
2014-11-19 11:16:46 +1030

    created basic extensions class to help simplify

---
+++ WiFindUs/Classes/NumericExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WiFindUs
+{
+    public static class NumericExtensions
+    {
+        public static double ToRadians(this double val)
+        {
+            return (Math.PI / 180) * val;
+        }
+
+        public static bool Tolerance(this double val, double other, double tolerance)
+        {
+            return Math.Abs(val - other) <= tolerance;
+        }
+
+        public static bool Tolerance(this double? val, double? other, double tolerance)
+        {
+            if (other.HasValue != val.HasValue
+                || (val.HasValue && Math.Abs(val.Value - other.Value) > tolerance))
+                return false;
+            return true;
+        }
+    }
+}
