47820406e46b2ba8b586260ee3a8c581e1b6a77a
Mark Gillard <marzer_@hotmail.com>
2014-11-19 11:45:25 +1030

    added base Dispatcher project

---
+++ WiFindUs.Eye.Dispatcher/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WiFindUs.Eye.Dispatcher
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main(String[] args)
+        {
+            WFUApplication.Name = "Dispatcher";
+            WFUApplication.Description = "An Eye program designed to help first-responders manage their personell.";
+            WFUApplication.ConfigFilePath += "|eye.conf|dispatcher.conf";
+            WFUApplication.UsesMutex = true;
+            WFUApplication.UsesMySQL = true;
+            WFUApplication.MainFormType = typeof(DispatcherForm);
+#if DEBUG
+            WFUApplication.InitialVerbosity = Debugger.Verbosity.Verbose;
+            WFUApplication.UsesConsoleForm = true;
+#else
+            WFUApplication.InitialVerbosity = Debugger.Verbosity.Information;
+#endif
+            WFUApplication.Run(args);
+        }
+    }
+}
