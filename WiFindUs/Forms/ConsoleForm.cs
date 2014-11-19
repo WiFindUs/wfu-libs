54c2543c0ec29e4f65402cb702497d9317d05971
Mark Gillard <marzer_@hotmail.com>
2014-11-19 11:16:23 +1030

    ported from DebuggerFrame.java

---
+++ WiFindUs/Forms/ConsoleForm.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WiFindUs
+{
+	public class ConsoleForm : Form
+	{
+		private ConsolePanel console = null;
+
+        /////////////////////////////////////////////////////////////////////
+        // CONSTRUCTORS
+        /////////////////////////////////////////////////////////////////////
+
+		public ConsoleForm()
+		{
+			if (DesignMode)
+				return;
+
+			SetStyle(ControlStyles.DoubleBuffer
+			| ControlStyles.AllPaintingInWmPaint
+			| ControlStyles.ResizeRedraw
+			| ControlStyles.OptimizedDoubleBuffer, true);
+
+			Text = WFUApplication.Name + " :: Console";
+
+			Controls.Add(console = new ConsolePanel()
+			{
+				Dock = DockStyle.Fill,
+			});
+		}
+
+        /////////////////////////////////////////////////////////////////////
+        // PROTECTED METHODS
+        /////////////////////////////////////////////////////////////////////
+
+		protected override void OnFormClosing(FormClosingEventArgs e)
+		{
+			if (e.Cancel = (e.CloseReason == CloseReason.UserClosing))
+				Hide();
+			else
+				base.OnFormClosing(e);
+		}
+
+		protected override void OnShown(EventArgs e)
+		{
+			base.OnShown(e);
+			console.Focus();
+		}
+	}
+}
