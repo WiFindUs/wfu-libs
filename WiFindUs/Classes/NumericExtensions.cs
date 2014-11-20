9df33fad7fc949280ac14701eb97034e7239f80c
Mark Gillard <marzer_@hotmail.com>
2014-11-21 05:47:09 +1030

    added decimal methods

--- WiFindUs/Classes/NumericExtensions.cs
+++ WiFindUs/Classes/NumericExtensions.cs
@@ -7,6 +7,10 @@ namespace WiFindUs
 {
     public static class NumericExtensions
     {
+        /////////////////////////////////////////////////////////////////////
+        // DOUBLES
+        /////////////////////////////////////////////////////////////////////
+
         public static double ToRadians(this double val)
         {
             return (Math.PI / 180) * val;
@@ -18,6 +22,28 @@ namespace WiFindUs
         }
 
         public static bool Tolerance(this double? val, double? other, double tolerance)
+        {
+            if (other.HasValue != val.HasValue
+                || (val.HasValue && Math.Abs(val.Value - other.Value) > tolerance))
+                return false;
+            return true;
+        }
+
+        /////////////////////////////////////////////////////////////////////
+        // DECIMALS
+        /////////////////////////////////////////////////////////////////////
+
+        public static decimal ToRadians(this decimal val)
+        {
+            return ((decimal)Math.PI / 180m) * val;
+        }
+
+        public static bool Tolerance(this decimal val, decimal other, decimal tolerance)
+        {
+            return Math.Abs(val - other) <= tolerance;
+        }
+
+        public static bool Tolerance(this decimal? val, decimal? other, decimal tolerance)
         {
             if (other.HasValue != val.HasValue
                 || (val.HasValue && Math.Abs(val.Value - other.Value) > tolerance))
