bd4e2ad5d169a1365d886ba69094f51f2c34e7a9
Mark Gillard <marzer_@hotmail.com>
2014-11-26 01:25:26 +1030

    brought over MathHelper from Java

---
+++ WiFindUs/MathHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WiFindUs
+{
+    public static class MathHelper
+    {
+        public static double Lerp(double start, double finish, double amount)
+        {
+            return (1.0 - amount) * start + amount * finish;
+        }
+
+        public static double Coserp(double start, double finish, double amount)
+        {
+            double prc = (1.0 - Math.Cos(amount * Math.PI)) * 0.5;
+            return start * (1.0 - prc) + finish * prc;
+        }
+    }
+}
