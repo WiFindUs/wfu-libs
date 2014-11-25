ba501f739743e6f69f20738ea66ee89c0f049b65
Mark Gillard <marzer_@hotmail.com>
2014-11-26 08:13:08 +1030

    Added base form four double-buffering etc

---
+++ WiFindUs/Forms/BaseForm.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using WiFindUs.Extensions;
+
+namespace WiFindUs.Forms
+{
+	public class BaseForm : Form
+	{
+        /////////////////////////////////////////////////////////////////////
+        // PROPERTIES
+        /////////////////////////////////////////////////////////////////////
+
+        public bool IsDesignMode
+        {
+            get
+            {
+                return DesignMode || this.IsDesignMode();
+            }
+        }
+
+        protected override bool ShowWithoutActivation
+        {
+            get { return true; }
+        }
+
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams cp = base.CreateParams;
+                if (!IsDesignMode)
+                    cp.ExStyle |= 0x02000000;  // Turn on WS_EX_COMPOSITED
+                return cp;
+            }
+        }
+
+        /////////////////////////////////////////////////////////////////////
+        // CONSTRUCTORS
+        /////////////////////////////////////////////////////////////////////
+
+        public BaseForm()
+        {
+            if (!IsDesignMode)
+            {
+                DoubleBuffered = true;
+                SetStyle(
+                    System.Windows.Forms.ControlStyles.UserPaint |
+                    System.Windows.Forms.ControlStyles.AllPaintingInWmPaint |
+                    System.Windows.Forms.ControlStyles.OptimizedDoubleBuffer,
+                    true);
+            }
+        }
+	}
+}
