1a11d8e93dc45156a971677a845f69ab3ec8093a
Mark Gillard <marzer_@hotmail.com>
2014-11-26 08:16:08 +1030

    catch-all IsDesignMode

---
+++ WiFindUs/Extensions/ComponentExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace WiFindUs.Extensions
+{
+    public static class ComponentExtensions
+    {
+        public static bool IsDesignMode(this Component comp)
+        {
+            return (comp.Site != null && comp.Site.DesignMode)
+                || LicenseManager.UsageMode == LicenseUsageMode.Designtime
+                || AppDomain.CurrentDomain.FriendlyName.Equals("DefaultDomain");
+        }
+    }
+}
