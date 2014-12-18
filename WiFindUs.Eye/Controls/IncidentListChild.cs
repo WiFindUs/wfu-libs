d1ee184ce1544a2ae1a1f545c2d4e944ede3207b
Mark Gillard <marzer_@hotmail.com>
2014-12-18 11:08:34 +1030

    added other entity list child types

---
+++ WiFindUs.Eye/Controls/IncidentListChild.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WiFindUs.Controls;
+using WiFindUs.Extensions;
+
+namespace WiFindUs.Eye.Controls
+{
+    public class IncidentListChild : EntityListChild
+    {
+        private Waypoint incident;
+
+        /////////////////////////////////////////////////////////////////////
+        // PROPERTIES
+        /////////////////////////////////////////////////////////////////////
+
+        [Browsable(false)]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+        public Waypoint Incident
+        {
+            get { return incident; }
+        }
+
+        /////////////////////////////////////////////////////////////////////
+        // CONSTRUCTORS
+        /////////////////////////////////////////////////////////////////////
+
+        public IncidentListChild(Waypoint incident)
+        {
+            if (incident == null)
+                throw new ArgumentNullException("incident", "Incident waypoint cannot be null!");
+            this.incident = incident;
+        }
+    }
+}
---
+++ WiFindUs.Eye/Controls/UserListChild.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WiFindUs.Controls;
+using WiFindUs.Extensions;
+
+namespace WiFindUs.Eye.Controls
+{
+    public class UserListChild : EntityListChild
+    {
+        private User user;
+
+        /////////////////////////////////////////////////////////////////////
+        // PROPERTIES
+        /////////////////////////////////////////////////////////////////////
+
+        [Browsable(false)]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+        public User User
+        {
+            get { return user; }
+        }
+
+        /////////////////////////////////////////////////////////////////////
+        // CONSTRUCTORS
+        /////////////////////////////////////////////////////////////////////
+
+        public UserListChild(User user)
+        {
+            if (user == null)
+                throw new ArgumentNullException("user", "User cannot be null!");
+            this.user = user;
+        }
+    }
+}
