098b501be8622c6104ade0f0a6d9a72286f9f466
Mark Gillard <marzer_@hotmail.com>
2014-12-27 00:27:11 +1030

    list-specific selection group handling

--- WiFindUs.Eye/Controls/EntityList.cs
+++ WiFindUs.Eye/Controls/EntityList.cs
@@ -10,6 +10,29 @@ namespace WiFindUs.Eye.Controls
 {
     public class EntityList : ThemedFlowLayoutPanel
     {
+        private ISelectableEntityGroup selectionGroup = null;
+        private ISelectableEntityGroup defaultSelectionGroup = new SelectableEntityGroup();
+        private List<ISelectableEntity> entities = new List<ISelectableEntity>();
+        
+        public ISelectableEntityGroup SelectionGroup
+        {
+            get
+            {
+                return selectionGroup ?? defaultSelectionGroup;
+            }
+            set
+            {
+                if (entities.Count > 0)
+                    throw new InvalidOperationException("You can only change the selection group of an entity list when it is empty!");
+                ISelectableEntityGroup newValue = value == defaultSelectionGroup ? null : value;
+                if (selectionGroup == newValue)
+                    return;
+                selectionGroup = newValue;
+                foreach (ISelectableEntity entity in entities)
+                    entity.SelectionGroup = SelectionGroup;
+            }
+        }
+        
         public EntityList()
         {
             SuspendLayout();
@@ -20,13 +43,30 @@ namespace WiFindUs.Eye.Controls
 
         protected override void OnControlAdded(ControlEventArgs e)
         {
-            SuspendLayout();
             base.OnControlAdded(e);
+
             EntityListItem elc = e.Control as EntityListItem;
             if (elc == null)
                 return;
+            if (entities.Contains(elc.Entity))
+                throw new ArgumentOutOfRangeException("e", "An EntityListItem representing that entity is already present in this EntityList!");
+            elc.Theme = Theme;
             elc.Width = ClientRectangle.Width - 1;
-            ResumeLayout(true);
+            entities.Add(elc.Entity);
+            elc.Entity.SelectionGroup = SelectionGroup;
+        }
+
+        protected override void OnControlRemoved(ControlEventArgs e)
+        {
+            base.OnControlRemoved(e);
+            
+            EntityListItem elc = e.Control as EntityListItem;
+            if (elc == null)
+                return;
+
+            entities.Remove(elc.Entity);
+            if (elc.Entity.SelectionGroup == defaultSelectionGroup)
+                elc.Entity.SelectionGroup = null;
         }
     }
 }
