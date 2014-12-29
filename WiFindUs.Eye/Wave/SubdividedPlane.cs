2c3b4e4342d560fb7df1236632e4a88f172de048
Mark Gillard <marzer_@hotmail.com>
2014-12-30 10:02:43 +1030

    created placeholder for terrain plane class

---
+++ WiFindUs.Eye/Wave/SubdividedPlane.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WaveEngine.Components.Primitives;
+
+namespace WiFindUs.Eye.Wave
+{
+    public class SubdividedPlane : Geometric
+    {
+
+    }
+}
