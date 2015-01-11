91af650ee4ef85fcdbd7a7fc3ae6c3b443cb8515
Mark Gillard <marzer_@hotmail.com>
2015-01-11 19:25:33 +1030

    created device group action subscriber

---
+++ WiFindUs.Eye.Dispatcher/DeviceGroupActionSubscriber.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WiFindUs.Eye.Dispatcher
+{
+    public class DeviceGroupActionSubscriber : IActionSubscriber
+    {
+        private Device[] devices;
+
+        public DeviceGroupActionSubscriber(params Device[] devices)
+        {
+            if (devices == null)
+                throw new ArgumentNullException("devices", "Devices cannot be null");
+            if (devices.Length <= 0)
+                throw new ArgumentOutOfRangeException("devices", "Devices array cannot be empty");
+            this.devices = devices;
+        }
+        
+        public bool ActionEnabled(uint index)
+        {
+            switch (index)
+            {
+                case 0: return true;
+                case 1: return true;
+                case 2: return true;
+                case 9: return true;
+            }
+            return false;
+        }
+
+        public Image ActionImage(uint index)
+        {
+            return null;
+        }
+
+        public string ActionText(uint index)
+        {
+            switch (index)
+            {
+                case 0: return "Dispatch All";
+                case 1: return "Zoom To";
+                case 2: return "Track All";
+                case 8: return "Cancel All";
+            }
+            return "";
+        }
+
+        public void ActionTriggered(uint index)
+        {
+            
+        }
+    }
+}
