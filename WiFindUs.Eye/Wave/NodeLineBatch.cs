a8de41ae525a191879bdf2cd489b92f12da07940
Mark Gillard <marzer_@hotmail.com>
2015-01-11 19:38:21 +1030

    adding node connection line s

---
+++ WiFindUs.Eye/Wave/NodeLineBatch.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using WaveEngine.Common.Graphics;
+using WaveEngine.Common.Math;
+using WaveEngine.Framework;
+using WaveEngine.Framework.Graphics;
+
+namespace WiFindUs.Eye.Wave
+{
+    public class NodeLineBatch : Drawable3D
+    {
+        private static readonly Regex REGEX_IPV4
+            = new Regex(@"^\s*([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})\s*$", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+        [RequiredComponent]
+        private NodeMarker nodeMarker;
+        private Node node;
+        private EyeMainForm eyeForm;
+        [RequiredComponent]
+        private Transform3D transform3D;
+        
+
+        protected override void Initialize()
+        {
+            node = nodeMarker.Node;
+            eyeForm = (WFUApplication.MainForm as EyeMainForm);
+        }
+
+        public override void Draw(TimeSpan gameTime)
+        {
+            foreach (DeviceMarker marker in nodeMarker.Scene.DeviceMarkers)
+            {
+                if (marker.Device == null || marker.Device.TimedOut)
+                    continue;
+                IPAddress addr = marker.Device.IPAddress;
+                if (addr == null)
+                    continue;
+                String addrString = addr.ToString();
+                if (addrString.Equals("0.0.0.0"))
+                    continue;
+                Match match = REGEX_IPV4.Match(addrString);
+                if (!match.Success)
+                    continue;
+                long nodeNum = -1;
+                if (!Int64.TryParse(match.Groups[3].Value, out nodeNum))
+                    continue;
+                if (nodeNum != node.Number)
+                    continue;
+                RenderManager.LineBatch3D.DrawLine(transform3D.Position, marker.Transform3D.Position, Color.Blue);
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+
+        }
+    }
+}
---
+++ WiFindUs.Eye/Wave/NodeMarker.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WaveEngine.Common.Math;
+using WaveEngine.Components.Graphics3D;
+using WaveEngine.Framework;
+using WaveEngine.Framework.Graphics;
+using WaveEngine.Framework.Physics3D;
+
+namespace WiFindUs.Eye.Wave
+{
+    public class NodeMarker : EntityMarker
+    {
+        private Node node;
+        [RequiredComponent]
+        private Transform3D transform3D;
+        [RequiredComponent]
+        private MaterialsMap materialsMap;
+
+        /////////////////////////////////////////////////////////////////////
+        // PROPERTIES
+        /////////////////////////////////////////////////////////////////////
+
+        public Node Node
+        {
+            get { return node; }
+        }
+
+        public override Transform3D Transform3D
+        {
+            get { return transform3D; }
+        }
+
+        public override MaterialsMap MaterialsMap
+        {
+            get { return materialsMap; }
+        }
+
+        /////////////////////////////////////////////////////////////////////
+        // CONSTRUCTORS
+        /////////////////////////////////////////////////////////////////////
+
+        private NodeMarker(Node node)
+            : base(node)
+        {
+            this.node = node;
+        }
+
+        public static Entity Create(Node node)
+        {
+            return new Entity() { IsActive = false, IsVisible = false }
+                .AddComponent(new Transform3D())
+                .AddComponent(new MaterialsMap(PlaceHolderMaterial))
+                .AddComponent(Model.CreatePyramid(10f))
+                .AddComponent(new ModelRenderer())
+                .AddComponent(new BoxCollider())
+                .AddComponent(new NodeMarker(node))
+                .AddComponent(new NodeLineBatch())
+                .AddChild(new Entity() { IsActive = false, IsVisible = false }
+                    .AddComponent(new Transform3D())
+                    .AddComponent(new MaterialsMap(SelectedMaterial))
+                    .AddComponent(Model.CreateTorus(18, 1, 10))
+                    .AddComponent(new ModelRenderer())
+                );
+        }
+
+        /////////////////////////////////////////////////////////////////////
+        // PROTECTED METHODS
+        /////////////////////////////////////////////////////////////////////
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+            node.OnNodeLocationChanged += OnNodeLocationChanged;
+            node.OnNodeTimedOutChanged += OnNodeTimedOutChanged;
+            UpdateMarkerState();
+        }
+
+        protected override void Update(TimeSpan gameTime)
+        {
+
+        }
+
+        /////////////////////////////////////////////////////////////////////
+        // PRIVATE METHODS
+        /////////////////////////////////////////////////////////////////////
+
+        private void OnNodeLocationChanged(Node node)
+        {
+            UpdateMarkerState();
+        }
+
+        private void OnNodeTimedOutChanged(Node node)
+        {
+            UpdateMarkerState();
+        }
+    }
+}
