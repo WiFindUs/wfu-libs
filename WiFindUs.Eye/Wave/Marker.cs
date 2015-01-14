f581101798f000f85f18546ef824c48d8678da16
Mark Gillard <marzer_@hotmail.com>
2015-01-15 04:58:47 +1030

    generic marker parent

---
+++ WiFindUs.Eye/Wave/Marker.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WaveEngine.Framework;
+using WaveEngine.Framework.Graphics;
+using WaveEngine.Framework.Physics3D;
+
+namespace WiFindUs.Eye.Wave
+{
+    public abstract class Marker : Behavior
+    {
+        protected const float MAX_SPIN_RATE = 5.0f;
+        private MapScene scene;
+        private Transform3D transform;
+
+        /////////////////////////////////////////////////////////////////////
+        // PROPERTIES
+        /////////////////////////////////////////////////////////////////////
+
+        public MapScene Scene
+        {
+            get { return scene; }
+        }
+
+        public Transform3D Transform3D
+        {
+            get { return transform; }
+        }
+
+        public abstract BoxCollider BoxCollider { get; }
+
+        public abstract bool Selected { get; set; }
+
+        /////////////////////////////////////////////////////////////////////
+        // PROTECTED METHODS
+        /////////////////////////////////////////////////////////////////////
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+            scene = Owner.Scene as MapScene;
+            transform = Owner.FindComponent<Transform3D>();
+        }
+    }
+}
