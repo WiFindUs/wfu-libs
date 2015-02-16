81dbf4544e3988f204ed0f7acb4f94706a20e0e7
Mark Gillard <marzer_@hotmail.com>
2015-02-17 00:35:25 +1030

    adding event handlers for node events

--- WiFindUs.Eye/Controls/NodeListItem.cs
+++ WiFindUs.Eye/Controls/NodeListItem.cs
@@ -52,6 +52,64 @@ namespace WiFindUs.Eye.Controls
             : base(node)
         {
             this.node = node;
+
+            node.LocationChanged += node_LocationChanged;
+            node.OnAPDaemonRunningChanged += node_RunningDaemonsChanged;
+            node.OnDHCPDaemonRunningChanged += node_RunningDaemonsChanged;
+            node.OnGPSDaemonRunningChanged += node_RunningDaemonsChanged;
+            node.OnMeshPointChanged += node_RunningDaemonsChanged;
+            node.OnMeshPeersChanged +=node_OnMeshPeersChanged;
+            node.OnNodeIPAddressChanged += node_OnNodeIPAddressChanged;
+            node.OnNodeNumberChanged += node_OnNodeNumberChanged;
+            node.OnNodeVoltageChanged += node_OnNodeVoltageChanged;
+            node.OnVisibleSatellitesChanged += node_OnVisibleSatellitesChanged;
+            node.TimedOutChanged += node_TimedOutChanged;
+            node.WhenUpdated += node_WhenUpdated;
+        }
+
+        private void node_WhenUpdated(IUpdateable obj)
+        {
+            this.RefreshThreadSafe();
+        }
+
+        private void node_TimedOutChanged(IUpdateable obj)
+        {
+            this.RefreshThreadSafe();
+        }
+
+        private void node_OnVisibleSatellitesChanged(Node obj)
+        {
+            this.RefreshThreadSafe();
+        }
+
+        private void node_OnNodeVoltageChanged(Node obj)
+        {
+            this.RefreshThreadSafe();
+        }
+
+        private void node_OnNodeNumberChanged(Node obj)
+        {
+            this.RefreshThreadSafe();
+        }
+
+        private void node_OnNodeIPAddressChanged(Node obj)
+        {
+            this.RefreshThreadSafe();
+        }
+
+        private void node_OnMeshPeersChanged(Node obj)
+        {
+            this.RefreshThreadSafe();
+        }
+
+        private void node_RunningDaemonsChanged(Node obj)
+        {
+            this.RefreshThreadSafe();
+        }
+
+        private void node_LocationChanged(ILocatable obj)
+        {
+            this.RefreshThreadSafe();
         }
 
         /////////////////////////////////////////////////////////////////////
