f36adb70a1a32c4ef6c9d77c2454eaa5e93b6adf
Mark Gillard <marzer_@hotmail.com>
2015-03-22 15:20:17 +1030

    added post-build to copy build output

---
+++ WiFindUs.Eye.Dispatcher/post-build-copy.bat
@@ -0,0 +1,14 @@
+SET INPUT_DIR=%1
+SET OUTPUT_DIR=%2
+
+IF %INPUT_DIR:~-1%==\ SET INPUT_DIR=%INPUT_DIR:~0,-1%
+IF %OUTPUT_DIR:~-1%==\ SET OUTPUT_DIR=%OUTPUT_DIR:~0,-1%
+
+ECHO input: %INPUT_DIR%
+ECHO output: %OUTPUT_DIR%
+
+xcopy /C /R /Y /D %INPUT_DIR%\*.dll %OUTPUT_DIR%
+xcopy /C /R /Y /D %INPUT_DIR%\*.exe %OUTPUT_DIR%
+xcopy /C /R /Y /D %INPUT_DIR%\*.conf %OUTPUT_DIR%
+xcopy /C /R /Y /D /S %INPUT_DIR%\images %OUTPUT_DIR%\images
+xcopy /C /R /Y /D /S %INPUT_DIR%\maps %OUTPUT_DIR%\maps
